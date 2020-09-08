9fffa662e59d2e3895e17c031d72ebbbbbe4c0ad
TheMichaelJiles <sjiles1@my.westga.edu>
2020-09-08 11:15:24 -0400

    luhn_verified() is now fully functional, needs to be tested

---
+++ cs3280project1.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+import utils
+
+print(utils.luhn_verified('5558397375275489'))
+print(utils.luhn_verified('30536723505217'))
+print(utils.luhn_verified('4573055613536303098'))
+print(utils.luhn_verified('4573055613536303099'))
+print(utils.luhn_verified('1234-1234'))
---
+++ utils.py
@@ -0,0 +1,40 @@
+
+
+def luhn_verified(credit_card_number):
+    '''
+    Returns 'Authentic' if credit_card_number is real, or 'Fake' if it is not.
+    '''
+
+    number_list = [int(i) for i in str(credit_card_number)]
+    print(number_list)
+    last_digit = number_list.pop()
+
+    number_list.reverse()
+
+    print(number_list)
+    for i in range(len(number_list)):
+        if (((i % 2) == 0)):
+            number = number_list[i]
+            number = number * 2
+            if(number > 9):
+                number = number - 9
+            number_list[i] = number
+    sum = 0
+
+    for number in number_list:
+        sum = sum + number
+
+    print(number_list)
+    print(sum)
+    if (sum % 10) != 10 - last_digit:
+        return 'Fake'
+
+    return 'Authentic'
+
+
+def is_valid(sequence):
+    '''
+    Returns true if the specified string sequence is a valid credit card number,
+    and false otherwise.
+    '''
+    return False
