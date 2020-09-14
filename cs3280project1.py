a807c33cbdafef0a96392b11c6c741a9860aad02
TheMichaelJiles <sjiles1@my.westga.edu>
2020-09-13 20:43:21 -0400

    Ready for py builder

--- cs3280project1.py
+++ cs3280project1.py
@@ -1,35 +1,52 @@
 #! /usr/bin/python
-import utils
+'''
+Program that allows validates credit card numbers and identifies what type
+of card the number is from.
+author: Michael Jiles
+Version: 1.0
+'''
 import sys
+import utils
 
 def main():
+    '''
+    The main method that drives the program. Allows user to enter a credit card number,
+    and outputs the type as well as if thenumber is authentic.
+    '''
     pad_length = 25
     card_types_path = sys.argv[1]
     card_types = load_card_types(card_types_path)
     card_number = input("Please enter a credit card number:")
-    if (not utils.is_valid(card_number)):
-        print(pad_spaces_to_length("Credit card number: ",pad_length) + "Invalid")
-        print(pad_spaces_to_length("Credit card type: ",pad_length) + "Invalid")
-        print(pad_spaces_to_length("Luhn Verification: ",pad_length) + "N/A")
+    if not utils.is_valid(card_number):
+        print(pad_spaces_to_length("Credit card number: ", pad_length) + "Invalid")
+        print(pad_spaces_to_length("Credit card type: ", pad_length) + "Invalid")
+        print(pad_spaces_to_length("Luhn Verification: ", pad_length) + "N/A")
     else:
-        print(pad_spaces_to_length("Credit card number: ",pad_length) + card_number)
+        print(pad_spaces_to_length("Credit card number: ", pad_length) + card_number)
         name = get_card_type(card_types, card_number)
-        print(pad_spaces_to_length("Credit card type: ",pad_length) + str(name))
-        print(pad_spaces_to_length("Luhn Verification: ",pad_length) + utils.luhn_verified(card_number))
+        print(pad_spaces_to_length("Credit card type: ", pad_length) + str(name))
+        print(pad_spaces_to_length("Luhn Verification: ", pad_length) +
+              utils.luhn_verified(card_number))
 
 def pad_spaces_to_length(string, length):
-    string_length = len(string)
-    for number in range(len(string),length):
+    '''
+    Takes in a string and a length, pads spaces onto the end
+    of the string until the length is reached.
+    '''
+    for _ in range(len(string), length):
         string += " "
     return string
 
 def get_card_type(card_types, card_number):
-    for type in card_types:
-        name = type[0]
-        lengths = type[1]
+    '''
+    Returns the card type based on the card_types datastructure
+    '''
+    for card_type in card_types:
+        name = card_type[0]
+        lengths = card_type[1]
         for length in lengths:
             if length == len(card_number):
-                prefixes = type[2]
+                prefixes = card_type[2]
                 for prefix in prefixes:
                     prefix_length = len(str(prefix))
                     if int(card_number[0:prefix_length]) == int(prefix):
@@ -38,6 +55,9 @@ def get_card_type(card_types, card_number):
 
 
 def load_card_types(filepath):
+    '''
+    Loads the card_types datastructure
+    '''
     file = open(filepath)
     lines = file.readlines()
     card_types = []
@@ -48,15 +68,12 @@ def load_card_types(filepath):
         length_list = []
         prefixes = line[2].split(',')
         prefix_list = []
-        card_type_tuple = (line[0])
         for length in lengths:
             length_list.append(int(length))
         for prefix in prefixes:
-            if('-' in prefix):
+            if '-' in prefix:
                 prefix_split = prefix.split('-')
-                start = int(prefix_split[0])
-                end = int(prefix_split[1])
-                prefix_range = list(range(start, end))
+                prefix_range = list(range(int(prefix_split[0]), int(prefix_split[1])))
                 for number in prefix_range:
                     prefix_list.append(number)
             else:
@@ -65,5 +82,5 @@ def load_card_types(filepath):
         card_types.append(card_tuple)
     return card_types
 
-if __name__ == ‘__main__’:
+if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+'''
+Utils that are used for validating credit card numbers
+author: Michael Jiles
+Version: 1.0
+'''
 import re
 
 def luhn_verified(credit_card_number):
@@ -5,27 +10,28 @@ def luhn_verified(credit_card_number):
     Returns 'Authentic' if credit_card_number is real, or 'Fake' if it is not.
     '''
 
-    if(not is_valid(credit_card_number)):
+    if not is_valid(credit_card_number):
         return 'N/A'
 
     number_list = [int(i) for i in str(credit_card_number)]
     last_digit = number_list.pop()
 
     number_list.reverse()
+    length = len(number_list)
 
-    for i in range(len(number_list)):
-        if (((i % 2) == 0)):
+    for i in range(length):
+        if (i % 2) == 0:
             number = number_list[i]
             number = number * 2
-            if(number > 9):
+            if number > 9:
                 number = number - 9
             number_list[i] = number
-    sum = 0
+    curr_sum = 0
 
     for number in number_list:
-        sum = sum + number
+        curr_sum = curr_sum + number
 
-    if (sum % 10) != 10 - last_digit:
+    if (curr_sum % 10) != 10 - last_digit:
         return 'Fake'
 
     return 'Authentic'
@@ -36,5 +42,5 @@ def is_valid(sequence):
     Returns true if the specified string sequence is a valid credit card number,
     and false otherwise.
     '''
-    regex = '(\d{13}\d{0,6})|(\d{4}(\-\d{4}){3})|(\d{4}(\s\d{4}){3})'
+    regex = r'(\d{13}\d{0,6})|(\d{4}(\-\d{4}){3})|(\d{4}(\s\d{4}){3})'
     return re.match(regex, str(sequence))
